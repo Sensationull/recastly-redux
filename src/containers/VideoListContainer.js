ece6207afc2f0756d473d4880608cb19b4371b66
andrewjohnso7 <andrewjohnso17@gmail.com>
2018-10-03 21:16:12 -0700

    Small change

--- src/containers/VideoListContainer.js
+++ src/containers/VideoListContainer.js
@@ -7,6 +7,8 @@ var VideoListContainer = connect(mapStateToProps)(VideoPlayer);
 //TODO: define a VideoListContainer component which will hook up your action
 // dispatchers with your VideoList component props.
 
+// handle action of clicking on a new video in the list
+
 const mapStateToProps = (state) => {
   return {
     videos: state.videoList,
