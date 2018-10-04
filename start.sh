0a5b473b99f691698525f33591492135c496e726
andrewjohnso7 <andrewjohnso17@gmail.com>
2018-10-03 21:18:42 -0700

    adding in remote update to start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 npm install -g live-server
 npm install -g babel-cli
+git remote add andrew  https://github.com/andrewjohnso7/hrsf104-recastly-redux.git
 babel . --out-dir compiled --presets=react --ignore=node_modules,compiled --source-maps inline -w &
 live-server &
 webpack -d --watch &
